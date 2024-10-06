49c4ad2245de2d3c1f0fb11067d682e560718c8c
Anjan Shomodder <thatanjan@gmail.com>
2024-10-06 13:34:49 +0600

    Add all the remaining project data

--- src/projectData.ts
+++ src/projectData.ts
@@ -47,9 +47,57 @@ const projects: Project[] = [
     sourceCode: 'https://github.com/thatanjan/spaceX-landing-page-clone-yt',
     technologies: ['HTML', 'CSS', 'JavaScript'],
   },
+  {
+    title: 'Tesla Landing Page Clone',
+    description:
+      'A clone of the Tesla landing page, made with HTML, CSS, and JavaScript and Fullpage.JS',
+    screenshots: [],
+    liveDemo: 'https://thatanjan.github.io/tesla-clone-yt',
+    sourceCode: 'https://github.com/thatanjan/tesla-clone-yt',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Fullpage.JS'],
+  },
+  {
+    title: 'Video Background Landing Page',
+    description:
+      'A beautiful landing page with a video background, made with HTML, CSS, and JavaScript.',
+    screenshots: [],
+    liveDemo: 'https://thatanjan.github.io/video-background-landing-page-yt/',
+    sourceCode: 'https://github.com/thatanjan/video-background-landing-page-yt',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+  },
+  {
+    title: 'Fullpage Landing Page',
+    description:
+      'A beautiful slide based fullpage landing page, made with HTML, CSS, JavaScript, and Fullpage.JS.',
+    screenshots: [],
+    liveDemo: 'https://thatanjan.github.io/fullpage-landing-page-yt',
+    sourceCode: 'https://github.com/thatanjan/fullpage-landing-page-yt',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'Fullpage.JS'],
+  },
+  {
+    title: 'Cules Shop',
+    description:
+      'Cules shop is an open source e-commerce website where people can buy and sell their electronic products.',
+    screenshots: [],
+    liveDemo: 'https://cules-shop.vercel.app/',
+    sourceCode: 'https://github.com/thatanjan/cules-shop',
+    technologies: [
+      'React',
+      'Next.js',
+      'MongoDB',
+      'Mongoose',
+      'TypeScript',
+      'GraphQL',
+      'Node.js',
+      'Express',
+      'Material-UI',
+      'GraphQL',
+    ],
+  },
 ]
 
 projects.forEach(project => {
+  console.log(project.title)
   project.screenshots = getScreenshotsPath(project.title)
 })
 
